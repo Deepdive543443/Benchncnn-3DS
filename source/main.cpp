b36456807a7df47f0281db9909c7615d807bc9af
justinfung <qf28@sussex.ac.uk>
2023-10-08 10:31:29 +0100

    We did it!

--- source/main.cpp
+++ source/main.cpp
@@ -37,6 +37,29 @@ public:
     }
 };
 
+double get_current_time()
+{
+#if (__cplusplus >= 201103L || (defined(_MSVC_LANG) && _MSVC_LANG >= 201103L)) && !defined(__riscv) && !NCNN_SIMPLESTL
+    auto now = std::chrono::high_resolution_clock::now();
+    auto usec = std::chrono::duration_cast<std::chrono::microseconds>(now.time_since_epoch());
+    return usec.count() / 1000.0;
+#else
+#ifdef _WIN32
+    LARGE_INTEGER freq;
+    LARGE_INTEGER pc;
+    QueryPerformanceFrequency(&freq);
+    QueryPerformanceCounter(&pc);
+
+    return pc.QuadPart * 1000.0 / freq.QuadPart;
+#else  // _WIN32
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+
+    return tv.tv_sec * 1000.0 + tv.tv_usec / 1000.0;
+#endif // _WIN32
+#endif
+}
+
 void benchmark(const char* comment, const ncnn::Mat &_in, const ncnn::Option& opt, bool fixed_path = true)
 {
     g_blob_pool_allocator.clear();
@@ -86,7 +109,9 @@ void benchmark(const char* comment, const ncnn::Mat &_in, const ncnn::Option& op
 
     for (int i = 0; i < g_loop_count; i++)
     {
-        // double start = ncnn::get_current_time();
+        // clock_t t;
+        // auto start = std::chrono::high_resolution_clock::now();
+        double start = get_current_time();
         {
             ncnn::Extractor ex = net.create_extractor();
             for (size_t j = 0; j < input_names.size(); ++j)
@@ -101,19 +126,23 @@ void benchmark(const char* comment, const ncnn::Mat &_in, const ncnn::Option& op
                 ex.extract(output_names[j], out);
             }
         }
+        // auto stop = std::chrono::high_resolution_clock::now();
+        double end = get_current_time();
 
-        // double end = ncnn::get
+        // auto duration = std::chrono::duration_cast<microseconds>(stop - start);
+        double time = end - start;
+        // double time_taken = ((double)t)/CLOCKS_PER_SEC;
+        // printf("fun() took %f seconds to execute \n", time_taken);
+        // std::cout << "Time: " << t << std::endl;
 
-        // double time = end - start;
-
-        // time_min = std::min(time_min, time);
-        // time_max = std::max(time_max, time);
-        // time_avg += time;
+        time_min = std::min(time_min, time);
+        time_max = std::max(time_max, time);
+        time_avg += time;
     }
 
     time_avg /= g_loop_count;
-    fprintf(stderr, "%20s  min = %7.2f  max = %7.2f  avg = %7.2f\n", comment, 114.514, 1919.810, time_avg);
-    // fprintf(stderr, "%20s  min = %7.2f  max = %7.2f  avg = %7.2f\n", comment, time_min, time_max, time_avg);
+
+    fprintf(stderr, "%20s  min = %7.2f  max = %7.2f  avg = %7.2f\n", comment, time_min, time_max, time_avg);
 }
 
 int main(int argc, char** argv)
@@ -197,7 +226,7 @@ int main(int argc, char** argv)
             // mat = mat.reshape(1, 1, 512);
             // pretty_print(mat);
 
-            benchmark("shufflenet_v2.param", ncnn::Mat(128, 128, 3), opt);
+            benchmark("shufflenet_v2.param", ncnn::Mat(64, 64, 3), opt);
             consoleSelect(&bottomScreen);
         }
     }
