42d788ef034966677115226cf97ed08632d5a5dd
Justin Fung <83911295+Deepdive543443@users.noreply.github.com>
2023-10-08 06:28:52 +0100

    Create make.sh

---
+++ source/make.sh
@@ -0,0 +1,7 @@
+export DEVKITPRO=/opt/devkitpro
+export DEVKITARM=/opt/devkitpro/devkitARM
+export DEVKITPPC=/opt/devkitpro/devkitPPC
+export PATH="/opt/devkitpro/tools/bin:$PATH"
+source ~/.profile
+
+make
